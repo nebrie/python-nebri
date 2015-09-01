1522cad895467eddaec2c1a522d07a55b4b3162e
Brie Marvin <briem@bixly.com>
2015-09-01 12:53:25 -0700

    added headers option

--- nebrios_client/nebrios_client.py
+++ nebrios_client/nebrios_client.py
@@ -6,7 +6,7 @@ class NebriOSClient(object):
     def __init__(self, instance_name):
         self.instance_name = instance_name
 
-    def api_request(self, api_module, view_name, method='GET', payload=None, files=None):
+    def api_request(self, api_module, view_name, method='GET', headers=None, payload=None, files=None):
         """
         :param api_module: the api module instantiated on the given instance
         :param view_name: a view in the given api module
@@ -17,17 +17,17 @@ class NebriOSClient(object):
         url = 'https://%s.nebrios.com/api/v1/%s/%s' % (self.instance_name, api_module, view_name)
         r = None
         if method == 'POST':
-            r = requests.post(url, data=payload, files=files)
+            r = requests.post(url, data=payload, files=files, headers=headers)
         elif method == 'GET':
-            r = requests.get(url, params=payload)
+            r = requests.get(url, params=payload, headers=headers)
         elif method == 'PUT':
-            r = requests.put(url, data=payload)
+            r = requests.put(url, data=payload, headers=headers)
         elif method == 'DELETE':
-            r = requests.delete(url)
+            r = requests.delete(url, headers=headers)
         elif method == 'HEAD':
-            r = requests.head(url)
+            r = requests.head(url, headers=headers)
         elif method == 'OPTIONS':
-            r = requests.options(url)
+            r = requests.options(url, headers=headers)
         else:
             return 'Incorrect Method.'
         if 'application/json' in r.headers['content-type']:
