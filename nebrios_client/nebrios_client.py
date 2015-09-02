ffc32768695f58d59049705a710dadcd3b9c4162
Brie Marvin <briem@bixly.com>
2015-09-02 10:14:58 -0700

    added auth to requests

--- nebrios_client/nebrios_client.py
+++ nebrios_client/nebrios_client.py
@@ -6,7 +6,7 @@ class NebriOSClient(object):
     def __init__(self, instance_name):
         self.instance_name = instance_name
 
-    def api_request(self, api_module, view_name, method='GET', headers=None, payload=None, files=None):
+    def api_request(self, api_module, view_name, method='GET', headers=None, payload=None, files=None, auth=None):
         """
         :param api_module: the api module instantiated on the given instance
         :param view_name: a view in the given api module
@@ -17,19 +17,20 @@ class NebriOSClient(object):
         url = 'https://%s.nebrios.com/api/v1/%s/%s' % (self.instance_name, api_module, view_name)
         r = None
         if method == 'POST':
-            r = requests.post(url, data=payload, files=files, headers=headers)
+            r = requests.post(url, data=payload, files=files, headers=headers, auth=auth)
         elif method == 'GET':
-            r = requests.get(url, params=payload, headers=headers)
+            r = requests.get(url, params=payload, headers=headers, auth=auth)
         elif method == 'PUT':
-            r = requests.put(url, data=payload, headers=headers)
+            r = requests.put(url, data=payload, headers=headers, auth=auth)
         elif method == 'DELETE':
-            r = requests.delete(url, headers=headers)
+            r = requests.delete(url, headers=headers, auth=auth)
         elif method == 'HEAD':
-            r = requests.head(url, headers=headers)
+            r = requests.head(url, headers=headers, auth=auth)
         elif method == 'OPTIONS':
-            r = requests.options(url, headers=headers)
+            r = requests.options(url, headers=headers, auth=auth)
         else:
             return 'Incorrect Method.'
+        print r
         if 'application/json' in r.headers['content-type']:
             return r.json()
         return r.content
